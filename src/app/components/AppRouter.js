ba04b287e5b179b862c903c1a89afb6a2508a5b5
Elliott Froigné <elliott.froigne@cgi.com>
2017-09-18 16:23:33 +0200

    New Page for the roller style

--- src/app/components/AppRouter.js
+++ src/app/components/AppRouter.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "app/pages/HomePage";
 import React from "react";
+import RollPage from "app/pages/RollPage";
 
 const NotFound = () => <h1>Page not found!</h1>;
 
@@ -8,6 +9,7 @@ const AppRouter = props => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={HomePage} />
+      <Route exact path="/roll" component={RollPage} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
