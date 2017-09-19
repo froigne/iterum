77aadec16eb9bf64e6b3b12fba2bc5a42a1a0528
Elliott Froigné <elliott.froigne@cgi.com>
2017-09-19 16:16:12 +0200

    Update animation shuffle

--- src/app/components/Shuffle/Shuffle.js
+++ src/app/components/Shuffle/Shuffle.js
@@ -3,17 +3,30 @@ import Mixer from "app/components/UI/Mixer";
 import PropTypes from "prop-types";
 import React from "react";
 import classes from "./Shuffle.module.css";
+import classnames from "classnames";
 
 export const Shuffle = ({ isShuffleFinish, shuffleResult, onShuffleProgress, ...props }) => (
   <div className={classes.shuffleContainer}>
     {!shuffleResult ? (
-      <Mixer replacements=")%/](€!\#[&?)" fps={60} factor={1}>
+      <Mixer className={classes.shuffle__mixer} replacements=")%/](€!\#[&?)" fps={60} factor={1}>
         Shuffle text
       </Mixer>
     ) : (
-      <div>
-        {isShuffleFinish ? <Button.Icon animate>thumb_up</Button.Icon> : ""}
-        <Mixer replacements=")%/](€!\#[&?)" fps={60} factor={10} onProgress={onShuffleProgress}>
+      <div
+        className={classnames(classes.shuffle__content, {
+          [classes.shuffle__done]: isShuffleFinish
+        })}
+      >
+        <div className={classes.shuffle__btnContainer}>
+          <Button.Icon className={classes.shuffle__btn__validate}>thumb_up</Button.Icon>
+        </div>
+        <Mixer
+          className={classes.shuffle__mixer}
+          replacements=")%/](€!\#[&?)"
+          fps={60}
+          factor={10}
+          onProgress={onShuffleProgress}
+        >
           {shuffleResult}
         </Mixer>
       </div>
