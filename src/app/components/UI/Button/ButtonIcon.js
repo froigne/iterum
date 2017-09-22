b97a6ba0bbe73641164b32320da57c99bf7fbe7c
Elliott Froigné <elliott.froigne@cgi.com>
2017-09-22 11:04:57 +0200

    Fix props button Icon

--- src/app/components/UI/Button/ButtonIcon.js
+++ src/app/components/UI/Button/ButtonIcon.js
@@ -4,14 +4,15 @@ import React from "react";
 import classes from "./Button.module.css";
 import classnames from "classnames";
 
-export const ButtonIcon = ({ className, ...props }) => (
-  <button className={classnames(className, classes.btnIcon)}>
-    <Icon {...props} />
+export const ButtonIcon = ({ className, children, ...props }) => (
+  <button className={classnames(className, classes.btnIcon)} {...props}>
+    <Icon children={children} />
   </button>
 );
 
 ButtonIcon.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  children: PropTypes.node
 };
 
 export default ButtonIcon;
