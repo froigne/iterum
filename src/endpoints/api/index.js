5ca12298bbc6bbd30a4a786d4b2f3e239749ded1
Elliott Froigné <elliott.froigne@cgi.com>
2017-10-23 10:32:26 +0200

    Fix api request

--- src/endpoints/api/index.js
+++ src/endpoints/api/index.js
@@ -1,18 +1,20 @@
 import { elementType } from "./schemas";
 import { generator } from "endpoints/lib";
+import createHttpClient from "fake-client/axios/createHttpClient";
+const fakeClient = createHttpClient();
 
 // elements
 const elementGenerator = generator.with("/elements", elementType);
 export const fetchElementList = elementGenerator.fetchEntityList;
 
 export const postElement = params => dispatch => {
-  const sendRequest = httpClient.post("elements", params);
+  const sendRequest = fakeClient.post("elements", params);
 
   return sendRequest;
 };
 
 export const deleteElement = entry => dispatch => {
-  const sendRequest = httpClient.delete(`elements/${entry}`);
+  const sendRequest = fakeClient.delete(`elements/${entry}`);
 
   return sendRequest;
 };
